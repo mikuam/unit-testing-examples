fd1443702a8b726b32ea189e0861ff2eaf89d5f0
Michal Bialecki <michal.bialecki@fabres.pl>
2019-01-01 12:47:34 +0100

    [minor] Added ProductNameProviderTests

---
+++ MichalBialecki.com.TestingExamples.ProductsProvider/ProductNameProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using MichalBialecki.com.TestingExamples.ProductsProvider.Tests;
+using NUnit.Framework;
+
+namespace MichalBialecki.com.TestingExamples.ProductsProvider
+{
+    [TestFixture]
+    public class ProductNameProviderTests
+    {
+        [Test]
+        public void GetProductName_GivenProductId_ReturnsProductName()
+        {
+            // Arrange
+            var productId = "1";
+            var productNameProvider = new ProductNameProvider();
+
+            // Act
+            var result = productNameProvider.GetProductName(productId);
+
+            // Arrange
+            Assert.AreEqual("Product " + productId, result);
+        }
+    }
+}
