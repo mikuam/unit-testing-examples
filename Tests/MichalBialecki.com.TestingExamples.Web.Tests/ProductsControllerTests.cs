a6b17b11fa23aeb0932cca114847bd3657053cab
Michal Bialecki <michal.bialecki@fabres.pl>
2019-01-03 11:23:04 +0100

    [minor] Added edge-cases tests

--- Tests/MichalBialecki.com.TestingExamples.Web.Tests/ProductsControllerTests.cs
+++ Tests/MichalBialecki.com.TestingExamples.Web.Tests/ProductsControllerTests.cs
@@ -40,9 +40,39 @@ namespace MichalBialecki.com.TestingExamples.Web.Tests
 
             // Assert
             Assert.AreEqual(result, guid);
+            _productService.Received(1).SaveProduct(product);
             _logger
                 .Received(1)
                 .Log(LogLevel.Information, 0, Arg.Is<FormattedLogValues>(v => v.First().Value.ToString().Contains(product.ProductId)), Arg.Any<Exception>(), Arg.Any<Func<object, Exception, string>>());
+        }
+
+        [Test]
+        public void Post_GivenNullProduct_ThrowsNullReferenceException()
+        {
+            // Act &  Assert
+            Assert.Throws<NullReferenceException>(() => _productsController.Post(null));
+        }
+
+        [Test]
+        public void Post_GivenNullProduct_ThrowsNullReferenceExceptionWithMessage()
+        {
+            // Act &  Assert
+            var exception = Assert.Throws<NullReferenceException>(() => _productsController.Post(null));
+            Assert.AreEqual("Object reference not set to an instance of an object.", exception.Message);
+        }
+
+        [Test]
+        public void Post_GivenNullProductId_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var guid = "af95003e-b31c-4904-bfe8-c315c1d2b805";
+            var product = new ProductDto { ProductId = null, ProductName = "Oven", QuantityAvailable = 3 };
+            _productService.SaveProduct(product).Returns(guid);
+
+            // Act &  Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => _productsController.Post(null));
+            Assert.True(exception.Message.Contains(nameof(ProductDto.ProductId)));
+
         }
     }
 }
